dc748abf1fb5c6624b1c98b9506923b51652346f
Artyom Chegodar <chegodar.artyom@gmail.com>
2025-01-08 15:04:14 +0300

    Created BuildingBlocks project

---
+++ src/BuildingBlocks/BuildingBlocks/Middlewares/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using BuildingBlocks.Exceptions;
+
+namespace BuildingBlocks.Middlewares;
+
+public class CustomExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<CustomExceptionHandlerMiddleware> _logger;
+
+    public CustomExceptionHandlerMiddleware(RequestDelegate next, ILogger<CustomExceptionHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = "Something went wrong";
+        switch (exception)
+        {
+            case ValidationException validationException:
+                code = HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(validationException.Message);
+                break;
+            case NotFoundException:
+                code = HttpStatusCode.NotFound;
+                break;
+            case ConflictException conflictException:
+                code = HttpStatusCode.Conflict;
+                result = JsonSerializer.Serialize(conflictException.Message);
+                break;
+            default:
+                _logger.LogError(exception, "An unexpected error occurred");
+                break;
+        }
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        if (result == string.Empty)
+        {
+            result = JsonSerializer.Serialize(new { error = exception.Message });
+        }
+
+        return context.Response.WriteAsync(result);
+    }
+}
