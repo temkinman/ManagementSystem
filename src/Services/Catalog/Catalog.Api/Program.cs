b11e04d6ff0a93c50d46ae921b2cceeec40d1b78
Chegodar Artyom <chegodar.artyom@gmail.com>
2025-01-10 19:47:54 +0300

    added swagger, project references, installed packages

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,12 +1,22 @@
+using BuildingBlocks.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
